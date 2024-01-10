a9daf818ad968ac143708cc26dc5a488886b9d85
Jesus Ramirez <jesusdevelp@gmail.com>
2024-01-09 19:50:22 -0600

    Desing chat screen and input chat

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,7 +1,25 @@
+import { GptMessage, TextMessageBox, TypingLoader } from "../../components"
+import { MyMessage } from '../../components/chat-bubbles/MyMessage';
 
 
 export const OrthographyPage = () => {
+
   return (
-    <div>OrthographyPage</div>
+    <div className="chat-container" >
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2" >
+          <GptMessage text="Hola, puedes decirme la hora" />
+          <MyMessage text="Hola mundo" />
+
+          <TypingLoader className='fade-in' />
+        </div>
+      </div>
+
+      <TextMessageBox 
+        onSendMessage={(mesage) => console.log(mesage)}
+        placeholder="Escribe aqui lo que deseas"
+        disabledCorrections
+      />
+    </div>
   )
 }
